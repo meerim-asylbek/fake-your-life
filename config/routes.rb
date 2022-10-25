eb711f9bf03dd1d7006dd1a6abcbf172ac1e1650
Meerim Asylbekova <meerim.asylbekova@gmail.com>
2022-10-25 18:36:50 +0200

    Changes: Add devise gem Create User model Disable turbo method for sign-in and sign-up views Add partial flashes view for alerts and notices Add authenticate_user method except for home

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "pages#home"
+  # authenticated(:user) do
+  #   root "atrists#index"
+  # end
+
+  # unauthenticated(:user) do
+  #   root "pages#landing"
+  # end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
