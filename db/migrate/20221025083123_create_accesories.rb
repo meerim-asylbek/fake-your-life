9a8265ae1e7db22da4a2ddbd35e9e24512fd05db
sebastian navarro <sebastiannavarro.fl@gmail.com>
2022-10-25 10:31:26 +0200

    add model accesorie

--- db/migrate/20221025083123_create_accesories.rb
+++ db/migrate/20221025083123_create_accesories.rb
@@ -1,7 +1,7 @@
 class CreateAccesories < ActiveRecord::Migration[7.0]
   def change
     create_table :accesories do |t|
-      t.string :picture_ul
+      t.string :picture_url
       t.string :name
       t.string :type
 
