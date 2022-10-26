3fd916cea6bc79d0e004e9f791845001558ddce2
sebastian navarro <sebastiannavarro.fl@gmail.com>
2022-10-26 20:39:23 +0200

    meerim impro

--- db/schema.rb
+++ db/schema.rb
@@ -88,10 +88,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_26_175202) do
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.bigint "artist_id", null: false
     t.bigint "customer_id", null: false
     t.index ["artist_id"], name: "index_reviews_on_artist_id"
     t.index ["customer_id"], name: "index_reviews_on_customer_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -109,12 +111,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_26_175202) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "artists", "users"
-  add_foreign_key "hires", "artists"
-  add_foreign_key "reviews", "artists"
-  add_foreign_key "reviews", "users"
   add_foreign_key "customers", "users"
   add_foreign_key "hires", "artists"
   add_foreign_key "hires", "customers"
   add_foreign_key "reviews", "artists"
   add_foreign_key "reviews", "customers"
+  add_foreign_key "reviews", "users"
 end
